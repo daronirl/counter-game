f77f7dc8b53918be84dbec981c09838ca7980923
John Prescott <48197513+daronirl@users.noreply.github.com>
2022-07-23 18:06:23 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const button = document.querySelectorAll('.btn');
+const buttonRoll = document.querySelector('.btn--roll');
+const buttonHold = document.querySelector('.btn--hold');
+const buttonNewGame = document.querySelector('.btn--new');
+
